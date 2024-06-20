ca7098740fa192bed92d6adba9c975e4974c0c43
ankitarrow <156340025+ankitarrow@users.noreply.github.com>
2024-06-20 15:42:59 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(cookieParser())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use("/api",router)
+
+const PORT = process.env.PORT||8080
+
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+    })
+})
