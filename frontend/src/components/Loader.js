f935065b4da0291413edaccb2e1dd036f870ac96
ankitarrow <156340025+ankitarrow@users.noreply.github.com>
2024-06-22 15:38:21 +0530

    Create Loader.js

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import fountain from '../assest/Fountain.gif'
+const Loader = () => {
+  return (
+    <div class="flex items-center justify-center ">
+    <img src={fountain} alt="Centered Image" class=""/>
+</div>
+
+  )
+}
+
+export default Loader
