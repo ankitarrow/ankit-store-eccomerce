5142ea5824945d7350e5535db291c43b031598ea
ankitarrow <156340025+ankitarrow@users.noreply.github.com>
2024-06-23 09:37:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,9 +74,8 @@ function App() {
         <ToastContainer 
           position='top-center'
         />
-<div className={`loader-container ${loader ? 'show' : ''}`}>
-  <Loader />
-</div>
+        {loader && <Loader  />}
+
         
         <Header/>
          <main className='min-h-[calc(100vh-120px)] pt-16'>
