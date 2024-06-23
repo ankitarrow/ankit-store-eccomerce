bec9839858a484ea2e7b045adcf50e5b16235fab
ankitarrow <156340025+ankitarrow@users.noreply.github.com>
2024-06-23 09:49:12 +0530

    Update Loader.js

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import fountain from '../assest/Fountain.gif'
 const Loader = () => {
   return (
-    <div class="flex items-center justify-center ">
+    <div class="flex items-center justify-center  " id="k">
     <img src={fountain} alt="Centered Image" class=""/>
 </div>
 
